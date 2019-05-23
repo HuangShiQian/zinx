81c224b1bcf8acb265b793ae18652023b812156a
HuangShiQian <17327735377@163.com>
2019-05-23 13:40:27 +0800

    ZinxDemo0523上午提交

---
+++ ZinxDemo/ZinxV0.2/server_main.go
@@ -0,0 +1,16 @@
+package main
+
+import "zinx/net"
+
+func main()  {
+	//创建一个zinx server对象
+	s:=net.NewServer("zinx v0.1")
+
+	//注册一些自定义的业务
+	//s.AddRouter(1, &dsad)
+
+	//让server对象 启动服务
+	s.Serve()
+
+	return
+}
